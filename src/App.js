5bb29ed43872ab9ed5893e49c1ca14ae6c83226e
Sandi Connors, M.A., CCC-SLP <123496920+smudgeConn@users.noreply.github.com>
2024-03-01 15:20:19 -0500

    Added the image files (unedited)

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
 import "./App.css";
 import Tile from "./Tile";
-import IIcon from "./images/I-icon.svg";
+import iIcon from "./images/meIcon.svg";
 import wantIcon from "./images/wantIcon.svg";
 import getIcon from "./images/getIcon.svg";
 import makeIcon from "./images/makeIcon.svg";
-import notIcon from "./images/notIcon.svg";
-import moreIcon from "./images/moreIcon.svg";
+// import notIcon from "./images/notIcon.svg";
+// import moreIcon from "./images/moreIcon.svg";
 import thatIcon from "./images/thatIcon.svg";
 import whoIcon from "./images/whoIcon.svg";
 import myIcon from "./images/myIcon.svg";
 import goIcon from "./images/goIcon.svg";
+import lookIcon from "./images/lookIcon.svg";
+import turnIcon from "./images/turnIcon.svg";
+import helpIcon from "./images/helpIcon.svg";
+import allIcon from "./images/allIcon.svg";
+// import hereIcon from "./images/hereIcon.svg";
+import whatIcon from "./images/whatIcon.svg";
+import youIcon from "./images/youIcon.svg";
+import likeIcon from "./images/likeIcon.svg";
+import openIcon from "./images/openIcon.svg";
+import doIcon from "./images/doingIcon.svg";
+import stopIcon from "./images/stopIcon.svg";
+import someIcon from "./images/fewIcon.svg";
+import inIcon from "./images/inIcon.svg";
+import whereIcon from "./images/whereIcon.svg";
+import itIcon from "./images/thatIcon.svg";
+// import canIcon from "./images/canIcon.svg";
+import playIcon from "./images/playingIcon.svg";
+import putIcon from "./images/putIcon.svg";
+import finishedIcon from "./images/finishedIcon.svg";
+// import onIcon from "./images/onIcon.svg";
+// import uhohIcon from "./images/uhohIcon.svg";
+// import heyIcon from "./images/heyIcon.svg";
 
 function App() {
   const wordArray = [
-    ["I", IIcon],
+    ["I", iIcon],
     ["want", wantIcon],
     ["get", getIcon],
     ["make", makeIcon],
-    ["not", notIcon],
-    ["more", moreIcon],
+    ["not"],
+    ["more"],
     ["that", thatIcon],
     ["who", whoIcon],
     ["mine", myIcon],
     ["go", goIcon],
-    ["look"],
-    ["turn"],
-    ["help"],
-    ["all"],
+    ["look", lookIcon],
+    ["turn", turnIcon],
+    ["help", helpIcon],
+    ["all", allIcon],
     ["here"],
-    ["what"],
-    ["you"],
-    ["like"],
-    ["open"],
-    ["do"],
-    ["stop"],
-    ["some"],
-    ["in"],
-    ["where"],
-    ["it"],
+    ["what", whatIcon],
+    ["you", youIcon],
+    ["like", likeIcon],
+    ["open", openIcon],
+    ["do", doIcon],
+    ["stop", stopIcon],
+    ["some", someIcon],
+    ["in", inIcon],
+    ["where", whereIcon],
+    ["it", itIcon],
     ["can"],
-    ["play"],
-    ["put"],
-    ["all done"],
+    ["play", playIcon],
+    ["put", putIcon],
+    ["finished", finishedIcon],
     ["on"],
     ["uh oh"],
     ["hey"],
   ];
 
   // feeling bored? move the wordArray to a new file
-  // consider using the kitten placeholder as the default setting in the Tile
 
   return (
     <>
