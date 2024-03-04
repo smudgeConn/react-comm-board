20f7c0c09e954f8f06ffc2fc059d44e207906a57
Sandi Connors, M.A., CCC-SLP <123496920+smudgeConn@users.noreply.github.com>
2024-03-04 17:15:45 -0500

    Finished updating the SVG files and got the sizing to work at a typical screen size for an 8x4 grid using flex-basis. Started working on the sentence bar component.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
+// NEXT: Update Python code to comment out the image text in the SVG files and update the images
+
 import "./App.css";
 import Tile from "./Tile";
+import SentenceBar from "./SentenceBar";
 import iIcon from "./images/meIcon.svg";
 import wantIcon from "./images/wantIcon.svg";
 import getIcon from "./images/getIcon.svg";
 import makeIcon from "./images/makeIcon.svg";
-// import notIcon from "./images/notIcon.svg";
-// import moreIcon from "./images/moreIcon.svg";
+import notIcon from "./images/notIcon.svg";
+import moreIcon from "./images/moreIcon.svg";
 import thatIcon from "./images/thatIcon.svg";
 import whoIcon from "./images/whoIcon.svg";
-import myIcon from "./images/myIcon.svg";
+import mineIcon from "./images/mineIcon.svg";
 import goIcon from "./images/goIcon.svg";
 import lookIcon from "./images/lookIcon.svg";
 import turnIcon from "./images/turnIcon.svg";
 import helpIcon from "./images/helpIcon.svg";
 import allIcon from "./images/allIcon.svg";
-// import hereIcon from "./images/hereIcon.svg";
+import hereIcon from "./images/hereIcon.svg";
 import whatIcon from "./images/whatIcon.svg";
 import youIcon from "./images/youIcon.svg";
 import likeIcon from "./images/likeIcon.svg";
 import openIcon from "./images/openIcon.svg";
-import doIcon from "./images/doingIcon.svg";
+import doIcon from "./images/doIcon.svg";
 import stopIcon from "./images/stopIcon.svg";
-import someIcon from "./images/fewIcon.svg";
+import someIcon from "./images/someIcon.svg";
 import inIcon from "./images/inIcon.svg";
 import whereIcon from "./images/whereIcon.svg";
-import itIcon from "./images/thatIcon.svg";
-// import canIcon from "./images/canIcon.svg";
-import playIcon from "./images/playingIcon.svg";
+import itIcon from "./images/itIcon.svg";
+import canIcon from "./images/canIcon.svg";
+import playIcon from "./images/playIcon.svg";
 import putIcon from "./images/putIcon.svg";
 import finishedIcon from "./images/finishedIcon.svg";
-// import onIcon from "./images/onIcon.svg";
-// import uhohIcon from "./images/uhohIcon.svg";
-// import heyIcon from "./images/heyIcon.svg";
+import onIcon from "./images/onIcon.svg";
+import uhohIcon from "./images/uhohIcon.svg";
+import heyIcon from "./images/heyIcon.svg";
 
 function App() {
   const wordArray = [
@@ -39,17 +42,17 @@ function App() {
     ["want", wantIcon],
     ["get", getIcon],
     ["make", makeIcon],
-    ["not"],
-    ["more"],
+    ["not", notIcon],
+    ["more", moreIcon],
     ["that", thatIcon],
     ["who", whoIcon],
-    ["mine", myIcon],
+    ["mine", mineIcon],
     ["go", goIcon],
     ["look", lookIcon],
     ["turn", turnIcon],
     ["help", helpIcon],
     ["all", allIcon],
-    ["here"],
+    ["here", hereIcon],
     ["what", whatIcon],
     ["you", youIcon],
     ["like", likeIcon],
@@ -60,13 +63,13 @@ function App() {
     ["in", inIcon],
     ["where", whereIcon],
     ["it", itIcon],
-    ["can"],
+    ["can", canIcon],
     ["play", playIcon],
     ["put", putIcon],
     ["finished", finishedIcon],
-    ["on"],
-    ["uh oh"],
-    ["hey"],
+    ["on", onIcon],
+    ["uh oh", uhohIcon],
+    ["hey", heyIcon],
   ];
 
   // feeling bored? move the wordArray to a new file
@@ -75,6 +78,10 @@ function App() {
     <>
       <header></header>
       <body>
+        <div className="sentence-bar">
+          <SentenceBar />
+        </div>
+
         <div className="tile-container">
           {wordArray.map((word) => {
             return <Tile imageURL={word[1]} tileDescription={word[0]} />;
--- src/SentenceBar.js
+++ src/SentenceBar.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const SentenceBar = () => {
   return (
-    <div>
-      <input type="text" placeholder="Enter a sentence" />
-      {/* <button>Submit</button> */}
-    </div>
+    <div className="sentence-bar">{/* Render draggable tiles here */}</div>
   );
 };
 
